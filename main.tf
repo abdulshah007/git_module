4afb8f5ebe49c457590b2a3b79aa0e48c3d295fe
abdulshah007 <abdulrace007@gmail.com>
2026-08-04 20:50:18 +0530

    Comment out instance module in main.tfok
    Comment out the instance module configuration.

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 
-module "instance" {
-  source      = "git::https://github.com/abdulshah007/git_module"
-  sgname      = var.sgname
-  cidr        = var.cidr
-  mytag       = var.mytag
-  amiid       = var.amiid
-  machinetype = var.machinetype
-  keyname     = var.keyname
-}
+# module "instance" {
+#  source      = "git::https://github.com/abdulshah007/git_module"
+#  sgname      = var.sgname
+#  cidr        = var.cidr
+#  mytag       = var.mytag
+#  amiid       = var.amiid
+#  machinetype = var.machinetype
+#  keyname     = var.keyname }
